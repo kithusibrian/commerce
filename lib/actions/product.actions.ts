fc78e46f04e80f5c96f49532338fa9ebecabd40e
kithusibrian <briaokm@gmail.com>
2025-03-25 13:36:25 +0300

    Updating the product cards

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -2,6 +2,7 @@
 
 import { connectToDatabase } from '@/lib/db'
 import Product from '@/lib/db/models/product.model'
+import { IProduct } from '@/lib/db/models/product.model'
 
 export async function getAllCategories() {
   await connectToDatabase()
@@ -34,3 +35,20 @@ export async function getProductsForCard({
     image: string
   }[]
 }
+// GET PRODUCTS BY TAG
+export async function getProductsByTag({
+    tag,
+    limit = 10,
+  }: {
+    tag: string
+    limit?: number
+  }) {
+    await connectToDatabase()
+    const products = await Product.find({
+      tags: { $in: [tag] },
+      isPublished: true,
+    })
+      .sort({ createdAt: 'desc' })
+      .limit(limit)
+    return JSON.parse(JSON.stringify(products)) as IProduct[]
+  }
