c1dc4e9d1957cfb9fcf2b8bcb7a8366c6e4adf51
kithusibrian <briaokm@gmail.com>
2025-04-07 09:20:31 +0300

    Minor changes

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,7 +8,7 @@ export default function NotFound() {
     <div className='flex flex-col items-center justify-center min-h-screen '>
       <div className='p-6 rounded-lg shadow-md w-1/3 text-center'>
         <h1 className='text-3xl font-bold mb-4'>Not Found</h1>
-        <p className='text-destructive'>Could not find requested resource</p>
+        <p className='text-destructive'>Could not find requested resource Mf</p>
         <Button
           variant='outline'
           className='mt-4 ml-2'
